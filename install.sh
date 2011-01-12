2b5378e6794f1f4546a769057a8409201cf62a68
Victor Engmark <victor.engmark@gmail.com>
2011-01-12 20:54:15 +0100

    Working single user installation

--- install.sh
+++ install.sh
@@ -12,13 +12,13 @@ full_path="$(readlink -fn -- "$0")" || fail
 dir="$(dirname -- "$full_path")" || fail
 
 # Move files in place
-cp -v -- "${dir}/common.sh" "${dir}/mount-image" "${dir}/umount-image" /usr/local/bin || fail
+sudo cp -- "${dir}/common.sh" "${dir}/mount-image" "${dir}/umount-image" /usr/local/bin || fail
 
 # Make the scripts executable
-chmod a+x -- /usr/local/bin/mount-image /usr/local/bin/umount-image || fail
+sudo chmod a+x -- /usr/local/bin/mount-image /usr/local/bin/umount-image || fail
 
 # Import the Nautilus settings (run for each user)
-gconftool-2 --load mount-image/nautilus-actions/* || fail
+gconftool-2 --load nautilus-actions.xml || fail
 
 # Success
 echo 'Installation succeeded. Please see the README file for usage instructions.'
